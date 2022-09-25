20dc2676edc503d661113e8dadb4a95fc0465e88
Alex <alex.l@viomedia.ru>
2022-09-26 02:46:36 +0300

    Добавил начальное отображение для загрузки файла и получения данных из него

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,68 +1,98 @@
 <template>
   <f7-page name="home">
     <!-- Top Navbar -->
-    <f7-navbar large :sliding="false">
-      <f7-nav-left>
-        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
-      </f7-nav-left>
-      <f7-nav-title sliding>runway_strip</f7-nav-title>
-      <f7-nav-right>
-        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
-      </f7-nav-right>
-      <f7-nav-title-large>runway_strip</f7-nav-title-large>
+    <f7-navbar :sliding="false">
+      <f7-nav-title>runway_strip</f7-nav-title>
     </f7-navbar>
     <!-- Toolbar-->
-    <f7-toolbar bottom>
-      <f7-link>Left Link</f7-link>
-      <f7-link>Right Link</f7-link>
-    </f7-toolbar>
-    <!-- Page content-->
-    <f7-block strong>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </f7-block>
-    <f7-block-title>Navigation</f7-block-title>
-    <f7-list>
-      <f7-list-item link="/about/" title="About"></f7-list-item>
-      <f7-list-item link="/form/" title="Form"></f7-list-item>
-    </f7-list>
 
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised popup-open="#my-popup">Popup</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
 
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="left">Left Panel</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="right">Right Panel</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
-
-    <f7-list>
+    <f7-list inline-labels no-hairlines-md class="list">
       <f7-list-item
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      ></f7-list-item>
-      <f7-list-item
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      ></f7-list-item>
-      <f7-list-item
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      ></f7-list-item>
+          title="Файл"
+      >
+        <template #after>
+          <input
+              id="inputFile"
+              type="file"
+              required
+              class="hide-input file"
+              ref="inputFile"
+              @change="handleInputFile"
+          />
+          <label for="inputFile" class="cursor-pointer label-file">
+            {{ nameFile }}
+            <f7-icon material="upload_file"/>
+          </label>
+        </template>
+      </f7-list-item>
+      <f7-button
+          type="submit"
+          class="mt10"
+          fill
+          preloader
+          :loading="isLoading"
+          :disabled="isDisabled || isLoading"
+          @click="loadFileData">Просчитать
+      </f7-button>
     </f7-list>
   </f7-page>
 </template>
+
+<script>
+import readXlsxFile from 'read-excel-file'
+
+export default {
+  data() {
+    return {
+      inputFile: null,
+      isLoading: false,
+      isDisabled: true,
+      nameFile: 'Файл не выбран'
+    }
+  },
+  mounted() {
+    if (this.$refs.inputFile) {
+      this.inputFile = this.$refs.inputFile;
+    }
+  },
+
+  methods: {
+    loadFileData() {
+      this.isLoading = true;
+      readXlsxFile(this.inputFile.files[0]).then((rows) => {
+        // this.isLoading = false;
+        this.workFileData(rows)
+      })
+    },
+
+    handleInputFile() {
+      this.isDisabled = !this.inputFile.files.length;
+      this.nameFile = this.inputFile.files[0]?.name || 'Файл не выбран'
+    },
+
+    workFileData(data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.list {
+  max-width: 600px;
+  margin: 50px auto;
+}
+
+.label-file {
+  transition: 0.3s;
+
+  &:hover {
+    color: red;
+  }
+}
+
+.file:valid + .label-file {
+  color: red;
+}
+</style>
